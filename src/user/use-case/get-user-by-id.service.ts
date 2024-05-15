eb13bbe37590bed9b0002da2944651025f5bdcaa
GillesDarjana <gilles.darjana@supdevinci-edu.fr>
2024-05-15 16:36:59 +0200

    Update get-user-by-id-service

--- src/user/use-case/get-user-by-id.service.ts
+++ src/user/use-case/get-user-by-id.service.ts
@@ -6,10 +6,6 @@ import { User } from '../entity/user.entity';
 Injectable();
 export class GetUserByIdService {
   constructor(
-    // on "injecte" le repository de l'entité Article
-    // dans la propriété articleRepository de la classe ArticleService
-    // pour pouvoir ensuite utiliser les méthodes du repository
-    // dans les méthodes de notre service
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
